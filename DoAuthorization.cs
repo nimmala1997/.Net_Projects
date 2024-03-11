4e46c4349337b49ac09267d59a7350e2ef935637
npavankalyan <nimmalapavankalyan1997@gmail.com>
2024-03-11 07:49:18 +0530

    Initial Commit

---
+++ DoAuthorization.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.Primitives;
+
+namespace FirstApp
+{
+    public class DoAuthorization : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (context.Request.Path == "/" && context.Request.Method == "POST")
+            {
+                StreamReader reader = new StreamReader(context.Request.Body);
+                string? body = await reader.ReadToEndAsync();
+
+                //Parse the request body from string into Dictionary
+                Dictionary<string, StringValues> queryDict = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+                if (queryDict["email"] == "email" && queryDict["Password"] == "password")
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Valid Login");
+                    await next(context);
+                 }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid Credentials");
+                    //await next(context);
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Please enter Credentials");
+            }
+        }
+
+        private bool isValidLogin(Dictionary<string, StringValues> queryDict)
+        {
+            return queryDict["email"] == "email" && queryDict["Password"][0] == "password";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using FirstApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddTransient(typeof(DoAuthorization));
+builder.Services.AddTransient<DoAuthorization>(sp =>
+{
+    return new DoAuthorization();
+});
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World! Nimmala");
+
+
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+
+    context.Response.Headers["MyKey"] = "My Value";
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("World");
+    await next(context);
+});
+
+app.UseMiddleware<DoAuthorization>();
+
+app.Run(async (HttpContext context) =>
+{
+
+    //context.Response.Headers["MyKey"] = "My Value";
+    //context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("World");
+});
+
+app.Run();
+
